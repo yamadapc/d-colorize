8499c64cd145bc4e4bc1f4fa5900ca82fc937af8
yamadapc <tacla.yamada@gmail.com>
2014-08-01 04:30:50 -0300

    Bump the version to v1.0.2
    This is simply to keep dub's README information in sync.

--- source/colorize/winterm.d
+++ source/colorize/winterm.d
@@ -2,7 +2,7 @@
  * Authors: ponce
  * Date: July 28, 2014
  * License: Licensed under the MIT license. See LICENSE for more information
- * Version: 1.0.1
+ * Version: 1.0.2
  */
 module colorize.winterm;
 
